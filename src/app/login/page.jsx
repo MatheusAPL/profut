5814a789405db6d942d66445147169005586ced9
AndreJr117 <andre_jr97@hotmail.com>
2023-09-21 22:05:24 -0300

    committed

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,4 +1,5 @@
 "use client";
+import Input from "../components/input";
 import{Form,Formik} from "formik";
 import React from 'react'
 
@@ -6,7 +7,19 @@ export default function Login() {
   return (
     <main className="min-h-screen flex items -center justify-center">
         <Formik>
-            {({ values }) => <Form noValidate> Teste </Form>}
+            {({ values }) => 
+                <Form noValidate>
+                    <Input 
+                     name="email" 
+                     type="email" 
+                     required />
+                    <Input 
+                     name="password" 
+                     type="password" 
+                     required 
+                     autoComplete="off"
+                    />  
+                </Form>}
         </Formik>
     </main>
   )
