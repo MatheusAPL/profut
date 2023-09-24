1092ef397cb0c6795f476aa16961ceb396cd646c
Andrejr117 <andre_jr97@hotmail.com>
2023-09-24 14:55:25 -0300

    login 1.0

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,26 +1,68 @@
 "use client";
 import Input from "../components/input";
-import{Form,Formik} from "formik";
+import Link from "next/link";
+import { Form, Formik } from "formik";
 import React from 'react'
+import Button from "../components/Button";
+import * as Yup from "yup";
+
 
 export default function Login() {
+
+  const initialValues = {
+    email: "",
+    password: "",
+  };
+
+  const validationSchema = Yup.object().shape({
+    email: 
+      Yup
+      .string()
+      .email("Digite um e-mail válido")
+      .required("O campo e-mail é obrigatório"),
+    password:
+      Yup
+      .string()
+      .required("O campo senha é obrigatório"),
+  });
+
+  async function handleSubmit() {
+
+  }
+
   return (
-    <main className="min-h-screen flex items -center justify-center">
-        <Formik>
-            {({ values }) => 
-                <Form noValidate>
-                    <Input 
-                     name="email" 
-                     type="email" 
-                     required />
-                    <Input 
-                     name="password" 
-                     type="password" 
-                     required 
-                     autoComplete="off"
-                    />  
-                </Form>}
-        </Formik>
+    <main className="min-h-screen flex items-center justify-center">
+      <Formik 
+        initialValues={initialValues} 
+        validationSchema={validationSchema}
+        onSubmit={handleSubmit}
+      >
+        {({ values }) => (
+          <Form noValidate className="flex flex-col gap-2 p-4 border rounded border-zinc-300 min-w-[300px] bg-white">
+            <Input
+              name="Email"
+              type="email"
+              required />
+            <Input
+              name="Senha"
+              type="password"
+              required
+              autoComplete="off"
+            />
+            <Button
+              type="submit"
+              text="Entrar"
+              className="bg-green-500 text-white rounded p-2 cursor-pointer"
+              />
+            <span className="text-xs text-zinc-500">
+              Não possui uma conta ? 
+              <strong className="text-zinc-700">
+                <Link href="/register"> Clique aqui e marque um golaço !!!</Link>
+              </strong>
+            </span>
+          </Form>
+          )}
+      </Formik>
     </main>
   )
 }
